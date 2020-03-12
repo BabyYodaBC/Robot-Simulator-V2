59e1ab0c0641eab5d211c37ed9303f2f2e51b8cf
Babyyodabc <rjwahba@gmail.com>
2020-03-12 11:59:42 -0400

    basics complete

--- index.js
+++ index.js
@@ -2,5 +2,43 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  const movesCon = document.getElementById("moves-container");
+
+  document.addEventListener("keydown", function(event) {
+    switch (event.keyCode) {
+      case 37:
+           const left = document.createElement('li')
+           left.innerText = "left"
+           movesCon.appendChild(left)
+         break;
+      case 38:
+          const up = document.createElement('li')
+          up.innerText = "up"
+          movesCon.appendChild(up)
+         break;
+      case 39:
+        const right = document.createElement('li')
+        right.innerText = "right"
+        movesCon.appendChild(right)
+         break;
+      case 40:
+        const down = document.createElement('li')
+        down.innerText = "down"
+        movesCon.appendChild(down)
+         break;
+      case 8:
+        movesCon.removeChild(document.getElementById("moves-container").lastChild);
+      break;
+   }
+  })
+
+  const moveButton = document.getElementById("move-button")
+
+  moveButton.addEventListener("click", function(event){
+    let movesArray = Array.from(document.getElementsByTagName("li"))
+    let current = movesArray[0]
+    move(current.innerText)
+    movesCon.removeChild(current)
+  })
+
 })
